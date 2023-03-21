3a127e1bec5c7da1eb5c57924825c55b0944c9ac
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-21 05:07:41 -0700

    USing printf and add a newline

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/**
+ * main - entry point
+ *
+ * Description: USing printf Funtion
+ * this program prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
