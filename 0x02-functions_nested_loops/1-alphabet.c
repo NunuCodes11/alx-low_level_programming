d974e3ba79e41803c35d309f70153a2ae6ab5393
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-21 15:37:35 -0700

    Function prints alphabet in lowcase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Lawal Hamzat A.
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase followed by a newline
+ *
+ * Return: 0 Success
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
