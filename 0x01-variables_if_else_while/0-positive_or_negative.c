d6c13a42364c031493f984f8244d14d28618abeb
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-21 07:13:00 -0700

    Postive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+/**
+ * main - entry point
+ *
+ * Description: program to determine if a number is positive or not
+ * Return: 0
+ */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project covers the usage of variables in C and introduces the if-else and while loops. The project contains several scripts that solve different tasks involving variables and loops.
