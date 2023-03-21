a05595c6bf2af44de7bb032baead81b1a5a0a585
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-21 04:54:55 -0700

    Using Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ * main - entry point
+ *
+ * Description: using puts function
+ * this program prints Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
