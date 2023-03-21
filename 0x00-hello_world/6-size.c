bb9a5d61425c67a90efacbe821caf29b33eb9c1f
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-21 05:32:22 -0700

    OPerator

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	float d;
 	long long int e;
 
-	printf("Size of a char:   %d byte(s)\n", (int)sizeof(c));
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
 	printf("Size of an int: %d byte(s)\n", (int)sizeof(a));
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(b));
 	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(e));
-	printf("Size of float: %d byte(s)\n", (int)sizeof(d));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(d));
 	return (0);
 }
