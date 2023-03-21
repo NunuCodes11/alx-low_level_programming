02068b8673e4d4819c17d022149209da224d5005
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-21 06:20:54 -0700

    using fputs function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/**
+ * main - entry point 
+ *
+ * Description: Using write function
+ * this program prints and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Return: 1
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
