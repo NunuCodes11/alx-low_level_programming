6ae9b460a44928bd0afffb69104e30bcc56526a4
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-22 06:39:12 -0700

    Task 5 completed

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+/*
+ * File: 5-sign.c
+ * Auth: Lawal Hamzat A.
+ */
+
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: value to be compared
+ *
+ * Return: 1 if the n is greater than 0 and return 0 is n is zero
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	_putchar(48);
+	return (0);
+}
