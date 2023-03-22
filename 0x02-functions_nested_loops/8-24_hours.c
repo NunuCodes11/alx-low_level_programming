77c794eff2e0e9b40751babc2ef68e219d94dfa0
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-22 09:09:08 -0700

    Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,12 +19,12 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(58);
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			putchar('\n');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(58);
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
