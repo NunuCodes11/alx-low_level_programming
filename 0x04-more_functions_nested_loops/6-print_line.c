ea7971642219527e3e442259f357ff69756284c9
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-23 19:27:34 -0700

    Print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,23 +7,25 @@
 
 /**
  * print_line - draws a straight line on the terminal
- * @n - vlaue to be compared
+ * @n: value to be compared
  *
  * Return: 0
  */
 
 void print_line(int n)
 {
+	int i = 0;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		while (n > 0)
+		while (i < n)
 		{
 			_putchar('_');
-			n--;
+			i++;
 		}
 	}
 }
