2ba0ed110374843a54d1d332c1d05d5503e442b6
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-23 18:06:55 -0700

    isupper"

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-isupper.c
+ * Auth: Lawal Hamzat
+ */
+
+#include "main.h"
+
+/**
+ * _isupper - checks if a a number is uppercase
+ * @c: the vlaue tobe compared
+ *
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 &&  c <= 90)
+	{
+		return (1);
+	}
+	return (0)
+}
