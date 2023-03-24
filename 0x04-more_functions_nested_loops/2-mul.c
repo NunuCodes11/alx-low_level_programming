772d86ee9bc7b1d5e3e828a7719c7eabb7d1c390
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-23 18:22:38 -0700

    Fuctions that multiple two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-mul.c
+ * Auth: Lawal Hamzat A.
+ */
+
+#include "main.h"
+
+/**
+ * mul - return multiple of integers
+ * @a: value to be multiplied
+ * @b: value to be multiplied
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
