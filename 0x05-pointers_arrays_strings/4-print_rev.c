ee5cb11ce5820eb2e6b24c33eb54ae740879708d
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-27 10:06:13 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+/*
+ * File: 4-print_rev.c
+ * Auth: Lawal Hamzat A.
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - return the reversed string
+ * @s: the pinter to char to be reverse
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
