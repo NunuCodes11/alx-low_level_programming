b2684b81ff17cca6ff4a4b9370eeb4d8a6fefaba
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-27 02:25:44 -0700

    POinter and array task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Lawal Hamzat A.
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - reset a value to 98
+ * @n: a pointer to the interger we want to set to 98
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
