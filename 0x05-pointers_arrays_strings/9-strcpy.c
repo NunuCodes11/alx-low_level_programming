fd7935b4bc7debed7488d159a34d41c9dd118c45
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-28 12:30:37 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+/*
+ * File: 9-strcpy.c
+ * Auth: Lawal Hamzat A.
+ */
+
+#include "main.h"
+
+/**
+ * _strcpy - Write a function that copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: the string to be copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0, i;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
