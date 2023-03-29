6e7075ee7b07500d4fc84e0ea391db62835c2bc8
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-29 09:25:05 -0700

    Concatenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-strcat.c
+ * Auth: Lawal Hamzat A.
+ */
+#include "main.h"
+/**
+ * _strcat - concatenates strings
+ * @dest: pointer of a string to be concatenated
+ * @src: pointer to char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while(dest[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+	dest[len] = '\0';
+	return(dest);
+}
