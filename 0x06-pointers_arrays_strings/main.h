abee281397fba80694e6afe80c950b011859372d
NunuCodes11 <lawalhamzat11@gmail.com>
2023-03-30 05:23:14 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,21 @@
 #define MAIN_H
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 
 
+
+
+
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+char *leet(char *);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
 #endif
