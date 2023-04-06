38dffb4200d9fd3ac516b79e2de982ad27b4d1e0
NunuCodes11 <lawalhamzat11@gmail.com>
2023-04-06 00:01:24 -0700

    name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+/*
+ * File: 0-whatsmyname.c
+ * Auth:LAwal Hamzat A.
+ */
 
-int main(int argc, char* argv[])
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: 0 if sucessful
+ */
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
