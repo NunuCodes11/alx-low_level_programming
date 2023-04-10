8580c4329f017132427e6dd6b2bbf83994b013a1
NunuCodes11 <lawalhamzat11@gmail.com>
2023-04-10 10:21:13 +0100

    Betty Screamed at me. Btw why can't i have more than 40 lines in a function

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,44 +13,41 @@
 int main(int argc, char *argv[])
 {
 	int cent, coins = 0;
-	
-		if (argc != 2)
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cent = atoi(argv[1]);
+	if (cent < 0)
+	{
+		printf("0\n");
+	}
+	while (cent > 0)
+	{
+		if (cent - 25 >= 0)
 		{
-			printf("Error\n");
-			return (1);
+			cent -= 25;
 		}
-		else
+		else if (cent - 10 >= 0)
 		{
-			cent = atoi(argv[1]);
-			if (cent < 0)
-			{
-				printf("0\n");
-			}
-			while (cent > 0)
-			{
-				if (cent - 25 >= 0)
-				{
-					cent -= 25;
-				}
-				else if (cent - 10 >= 0)
-				{
-					cent -= 10;
-				}
-				else if (cent - 5 >= 0)
-				{
-					cent -= 5;
-				}
-				else if (cent - 2 >= 0)
-				{
-					cent -= 2;
-				}
-				else if(cent - 1 >= 0)
-				{
-					cent -= 1;
-				}
-				coins++;
-			}
-			printf("%d\n", coins);
-			return (0);
+			cent -= 10;
 		}
+		else if (cent - 5 >= 0)
+		{
+			cent -= 5;
+		}
+		else if (cent - 2 >= 0)
+		{
+			cent -= 2;
+		}
+		else if (cent - 1 >= 0)
+		{
+			cent -= 1;
+		}
+			coins++;
+	}
+	printf("%d\n", coins);
+	return (0);
 }
