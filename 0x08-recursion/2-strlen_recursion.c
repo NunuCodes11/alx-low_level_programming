2dcdc9f24454a34ab7078bbc247b6ddee98eb954
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-01 14:14:25 +0100

    strlen function using Reucursion)

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+/*
+ * File Name: 2-strlen_recursion.c
+ * Auth: Lawal Hamzat A.
+ */
+#include "main.h"
+
+/**
+ * _strlen_recursion - return the length of a string
+ *
+ * @s: pointer to a string
+ * Return: the length of a string
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
