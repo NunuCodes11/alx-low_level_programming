941db2987d29eae88145a4e430f251ea37989919
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-03 13:07:57 +0100

    pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - searchs for a character in a string
+ *
+ * @s: the string
+ * @c: the character
+ * Return: a pointer to the first occurrence of the character
+ */
+#include "main.h"
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return ('\0');
+}
