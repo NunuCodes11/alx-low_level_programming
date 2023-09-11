feb276ce9248989d846e75167d6aaaa7b120a78f
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-11 17:31:07 +0100

    Predifined Macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+/*
+* File: 2-main.c
+* Auth: Lawal Hamzat A.
+*/
+
+#include <stdio.h>
+/**
+* main - entry point
+* 
+* Return: 0 if successful
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
