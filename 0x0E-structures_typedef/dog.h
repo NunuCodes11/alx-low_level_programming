fd3309e5a3ca2e1cca463eabca556e85026a9572
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-13 04:00:09 +0100

    pushed my header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,16 @@
 * @age: Age of the day
 * @owner: the owner of the dog
 */
-
+#include <stdlib.h>
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 
 #endif
