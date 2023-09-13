e54b84dd79e974df35501c62a718f4cfe711f64f
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-13 04:33:07 +0100

    Done with Struct Tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,7 +7,7 @@
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog (Success), NULL otherwise
+ * Return: pointer if successful, NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
