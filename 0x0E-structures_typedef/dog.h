8d52f16050680d079d87ed3a8c5325ab3d902610
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-13 03:51:59 +0100

    Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a dog's basic info
+* @name: dog's name
+* @age: Age of the day
+* @owner: the owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
