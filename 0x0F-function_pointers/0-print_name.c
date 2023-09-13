5b0bdf532ecc514b50953161f350a2a3e1525136
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-13 18:37:56 +0100

    Fixed betty errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,10 @@
+/**
+* print_name - prints a name using func pointer
+*
+* @name: name to be printed
+* @f: function pointer to functions
+*/
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	f(name);
 }
