32b926bfe31c95c0d43615b7cb2c77dc3d66bf93
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-14 02:58:11 +0100

    pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,20 +1,16 @@
-/*
- * File: 3-main.c
- * Auth: Lawal Hamzat A.
- */
 #include "3-calc.h"
+
 /**
- * main - entry point
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * @argc: arguments count
- * @argv: array of strings
- * Return: 0 if sucessful
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-
-	int num1, num2, result;
-	char operator;
+	int arg1, arg2, result;
+	char o;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -23,21 +19,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
 	func = get_op_func(argv[2]);
+
 	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	operator = *argv[2];
-	if ((operator == '/' || operator == '%') && num2 == 0)
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	result = func(num1, num2);
+
+	result = func(arg1, arg2);
+
 	printf("%d\n", result);
+
 	return (0);
 }
