65db310294e0359c3caa04e18617919e664b6028
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-15 03:10:46 +0100

    Solution for Task 3ish

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,6 +28,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
-	va_end(string);
 	printf("\n");
+	va_end(string);
 }
