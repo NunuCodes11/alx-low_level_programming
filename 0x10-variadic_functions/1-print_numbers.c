78ef8ede6fb5800a6458bc6708c5b48067c95f90
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-15 02:10:38 +0100

    solution for task 1"

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+/*
+ * File: 1-print_numbers.c
+ * Auth: Lawal Hamzat A.
+ */
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  * print_numbers - prints number follwed by a new line
+  * @separator: string to be printed with the numbers
+  * @n: argument count
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int num;
+	unsigned int i;
+	va_list numbers;
+	va_start(numbers, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(numbers, int);
+		if (separator != NULL)
+		{
+			if (i == 0)
+			{
+				printf("%d", num);
+				continue;
+			}
+			printf("%s%d", separator, num);
+		}
+	}
+	va_end(numbers);
+	printf("\n");
+}
+
