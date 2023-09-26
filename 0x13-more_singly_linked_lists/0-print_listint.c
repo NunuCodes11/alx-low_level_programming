003a97640e68c2ec0094a4bf5152497fa1cd1049
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-26 19:51:20 +0100

    Print Single link list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+	
+	while (h != NULL)
+	{
+		len++;
+		printf("%d\n",h->n);
+		h = h->next;	
+	}
+	return (len);
+}
