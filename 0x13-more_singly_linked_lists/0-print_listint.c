404f6a2f282b584d3532b6a3427f556f80109986
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-26 19:56:56 +0100

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,18 @@
+/**
+  * print_listint - prints linked list
+  * @h: the pointer to the list
+  * Return: the length
+  */
 #include "lists.h"
 size_t print_listint(const listint_t *h)
 {
 	size_t len = 0;
-	
+
 	while (h != NULL)
 	{
 		len++;
-		printf("%d\n",h->n);
-		h = h->next;	
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (len);
 }
