b8367eefc35dfd20d8e1a699814a34d8d704e779
NunuCodes11 <lawalhamzat11@gmail.com>
2023-10-21 15:28:10 +0100

    bit manupulation mandatory tasks

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,11 +12,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int num = 1, general = n ^ m;
 	unsigned int flipped = 0;
 
-	while (one != 0)
+	while (num != 0)
 	{
 		if ((general & num) != 0)
 			flipped++;
-		num = one << 1;
+		num = num << 1;
 	}
 	return (flipped);
 }
